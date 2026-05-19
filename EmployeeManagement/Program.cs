5583d60dbeeaddfb47c11ccdd1e9f5a715eeb6d4
foram-evision <foram@evisionits.com>
2026-05-19 15:02:09 +0530

    Completed Phase 4 practical

---
+++ EmployeeManagement/Program.cs
@@ -0,0 +1,31 @@
+﻿List<Employee> employees = new List<Employee>()
+{
+    new Employee
+    {
+        Id = 1,
+        Name = "Rahul",
+        Department = "IT",
+        Salary = 50000
+    },
+
+    new Employee
+    {
+        Id = 2,
+        Name = "Priya",
+        Department = "HR",
+        Salary = 45000
+    },
+
+    new Employee
+    {
+        Id = 3,
+        Name = "Amit",
+        Department = "IT",
+        Salary = 70000
+    }
+};
+
+var emp =
+employees.FirstOrDefault(e => e.Id == 1);
+
+Console.WriteLine(emp.Name);
